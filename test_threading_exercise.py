04b8b7dbf40a9afcd345cdc574978d44583158e4
wis-janusz <wis.janusz@gmail.com>
2023-02-07 17:26:37 +0100

    -.-

--- test_threading_exercise.py
+++ test_threading_exercise.py
@@ -2,7 +2,7 @@ import pytest
 import numpy as np
 from threading_exercise import power_multiprocess, power_multitple_threads, power_single_thread
 
-_ARR_SIZE = 100000
+_ARR_SIZE = 10
 
 @pytest.fixture
 def arr_to_test(): return list(range(_ARR_SIZE))
@@ -15,11 +15,11 @@ def test_setup(arr_to_test, benchmark, rand_arr):
     pass
 
 
-def test_power_single_thread(benchmark, arr_to_test):
-    benchmark(power_single_thread,arr_to_test)
+def test_power_single_thread(benchmark):
+    benchmark(power_single_thread)
         
-def test_power_multitple_threads(benchmark,arr_to_test):
-    benchmark(power_multitple_threads,arr_to_test)
+def test_power_multitple_threads(benchmark):
+    benchmark(power_multitple_threads)
 
-def test_power_multiprocess(benchmark, arr_to_test):
-    benchmark(power_multiprocess,arr_to_test)
+def test_power_multiprocess(benchmark):
+    benchmark(power_multiprocess)
--- threading_exercise.py
+++ threading_exercise.py
@@ -1,15 +1,50 @@
 import threading
 import multiprocessing
 
+_ARR_SIZE = 10
+_arr = list(range(_ARR_SIZE))
+_index = 0
 
-def pow_list(arr:list[int]) -> list:
+def pow_list(lock:threading.Lock) -> list:
+    global _index
+    while True:
+        lock.acquire()
+        if _index < len(_arr):
+            _arr[_index] = _arr[_index]**2
+            _index += 1
+            print(threading.current_thread().name, _index)
+            lock.release()
+        else:
+            lock.release()
+            return
+        
+        
+    
+
+def power_single_thread() -> list:
+    
+    global _index 
+    _index = 0
+    lock = threading.Lock()
+    my_thread = threading.Thread(name='pow_list', target=pow_list, args=(lock,))
+    my_thread.start()
+    my_thread.join()
     return []
 
-def power_single_thread(arr:list[int]) -> list:
+
+def power_multitple_threads() -> list:
+    
+    global _index 
+    _index = 0
+    lock = threading.Lock()
+    my_threads = [threading.Thread(name=f't{x}', target=pow_list, args=(lock,)) for x in range(3)]
+    
+    for thread in my_threads:
+        print(thread)
+        thread.start()
+    for thread in my_threads:
+        thread.join()     
     return []
 
-def power_multitple_threads(arr:list[int]) -> list:
-    return []
-
-def power_multiprocess(arr:list[int]) -> list:
+def power_multiprocess() -> list:
     return []
